09ad7b34522a75f2e52cd50867cdd891d8a9e28b
Brice Rising <brice.rising@slalom.com>
2018-09-30 17:42:43 -0500

    Fix path of catalog-service

--- catalog-service/cicd-scripts/build.sh
+++ catalog-service/cicd-scripts/build.sh
@@ -1,4 +1,4 @@
-cd ../catalog-service
+cd catalog-service
 ./build.sh
 export CATALOG_PACKAGE_VERSION=`jq -r ".version" < package.json`
 docker tag catalog-service:latest docker.io/bricerisingslalom/catalog-service:$CATALOG_PACKAGE_VERSION
