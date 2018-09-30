df84a7b143251aa2dad3a8201cfaae72db4bc33d
Brice Rising <brice.rising@slalom.com>
2018-09-30 17:46:49 -0500

    add latest tags to builds

--- catalog-service/cicd-scripts/build.sh
+++ catalog-service/cicd-scripts/build.sh
@@ -2,5 +2,7 @@ cd catalog-service
 ./build.sh
 export CATALOG_PACKAGE_VERSION=`jq -r ".version" < package.json`
 docker tag catalog-service:latest docker.io/bricerisingslalom/catalog-service:$CATALOG_PACKAGE_VERSION
+docker tag catalog-service:latest docker.io/bricerisingslalom/catalog-service:latest
 docker push docker.io/bricerisingslalom/catalog-service:$CATALOG_PACKAGE_VERSION
+docker push docker.io/bricerisingslalom/catalog-service:latest
 cd -
